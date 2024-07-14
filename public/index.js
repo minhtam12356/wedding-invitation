4e8948105d5350b3668695ddb895f604851a50e3
Tam Nguyen <tam26918@gmail.com>
2024-07-14 11:29:11 +0700

    final

--- public/index.js
+++ public/index.js
@@ -7,18 +7,17 @@ function generateImage({ number, parentNode }) {
 
 const urlParams = new URLSearchParams(window.location.search);
 const queryName = urlParams.get("name") ?? "";
-const queryDate = urlParams.get("date") ?? 15;
+const queryDate = urlParams.get("date");
 
 const guestNamePosition = document.getElementsByClassName("guest_name")[0];
 const weddingPosition1 = document.getElementsByClassName("wedding-1")[0];
 const weddingPosition2 = document.getElementsByClassName("wedding-2")[0];
 const weddingPosition3 = document.getElementsByClassName("wedding-3")[0];
+const weddingPosition4 = document.getElementsByClassName("wedding-4")[0];
 const mainIntroductionPosition =
   document.getElementsByClassName("main_introduction")[0];
 const restaurantAddressPosition = document.getElementsByClassName("address")[0];
-const guestName = queryName ? Base64.decode(queryName) : "";
-
-guestNamePosition.innerHTML = guestName;
+const copywriterPosition = document.getElementsByClassName("copywriter")[0];
 
 let restaurantAddress = "";
 
@@ -27,27 +26,63 @@ const crystalPalace =
 const phoDoiPalace =
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.6250278412995!2d106.74569071464876!3d10.916075616651447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d9a5821d8013%3A0x1275a20bff991f23!2zUEjhu5AgxJDDlEkgUEFMQUNF!5e0!3m2!1sen!2s!4v1718210052201!5m2!1sen!2s";
 
-if (+queryDate === 15) {
-  restaurantAddress = phoDoiPalace;
-} else {
-  restaurantAddress = crystalPalace;
+switch (+queryDate) {
+  case 15:
+    restaurantAddress = phoDoiPalace;
+    break;
+
+  case 22:
+    restaurantAddress = crystalPalace;
+    break;
+
+  default:
+    break;
 }
 
-const iframe = document.createElement("iframe");
-iframe.setAttribute("width", "100%");
-iframe.setAttribute("height", "450");
-iframe.setAttribute("style", "border:0;");
-iframe.setAttribute("src", restaurantAddress);
+if (+queryDate === 15 || +queryDate === 22) {
+  const iframe = document.createElement("iframe");
+  iframe.setAttribute("width", "100%");
+  iframe.setAttribute("height", "450");
+  iframe.setAttribute("style", "border:0;");
+  iframe.setAttribute("src", restaurantAddress);
 
-restaurantAddressPosition.appendChild(iframe);
+  restaurantAddressPosition.appendChild(iframe);
 
-// -----------------------------------------------------------------------
-generateImage({ number: 1, parentNode: weddingPosition1 });
-generateImage({ number: 2, parentNode: weddingPosition2 });
-generateImage({ number: 3, parentNode: weddingPosition3 });
+  // -----------------------------------------------------------------------
+  generateImage({ number: 1, parentNode: weddingPosition1 });
+  generateImage({ number: 2, parentNode: weddingPosition2 });
+  generateImage({ number: 3, parentNode: weddingPosition3 });
 
-// -----------------------------------------------------------------------
-const imageIntroduction = document.createElement("img");
-imageIntroduction.setAttribute("width", "100%");
-imageIntroduction.setAttribute("src", `./assets/videos/${+queryDate}/1.gif`);
-mainIntroductionPosition.appendChild(imageIntroduction);
+  // -----------------------------------------------------------------------
+  const introductionImage = document.createElement("img");
+  introductionImage.setAttribute("width", "100%");
+  introductionImage.setAttribute("src", `./assets/videos/${+queryDate}/1.gif`);
+  mainIntroductionPosition.appendChild(introductionImage);
+
+  // -----------------------------------------------------------------------
+  const guestName = queryName ? Base64.decode(queryName) : "";
+  guestNamePosition.innerHTML = guestName;
+
+  // -----------------------------------------------------------------------
+  const thankyouImage = document.createElement("img");
+  thankyouImage.setAttribute("src", `./assets/images/4.png`);
+  weddingPosition4.appendChild(thankyouImage);
+
+  // -----------------------------------------------------------------------
+  const copywriterText = document.createElement("span");
+  copywriterText.innerHTML = "Created by Hoa Bui & Tam Nguyen";
+  copywriterPosition.appendChild(copywriterText);
+  copywriterPosition.setAttribute(
+    "style",
+    "padding-top: 0.4rem; padding-bottom: 1rem;"
+  );
+} else {
+  // -----------------------------------------------------------------------
+  const copywriterText = document.createElement("span");
+  copywriterText.innerHTML = "Link bị lỗi, đám cưới chúng em diễn ra vào ngày 15 và 22 tháng 9 năm 2024, Anh/Chị vui lòng liên hệ admin ạ!";
+  copywriterPosition.appendChild(copywriterText);
+  copywriterPosition.setAttribute(
+    "style",
+    "color: red; padding-top: 0.8rem; padding-bottom: 1rem;"
+  );
+}
